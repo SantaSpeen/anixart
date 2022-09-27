9f5016820545518994901e45822fdd7ad7cb9912
SantaSpeen <SantaSpeen@yandex.ru>
2022-09-27 20:56:40 +0300

    Немного изменил инфу о себе

--- anixart/__version__.py
+++ anixart/__version__.py
@@ -3,9 +3,9 @@
 __title__ = 'anixart'
 __description__ = 'Wrapper for using the Anixart API.'
 __url__ = 'https://github.com/SantaSpeen/anixart'
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 __build__ = 1
-__author__ = 'Maxim Khomutov'
+__author__ = 'SantaSpeen'
 __author_email__ = 'SantaSpeen@gmail.com'
 __license__ = "FPA"
-__copyright__ = 'Copyright 2022 Maxim Khomutov'
+__copyright__ = 'Copyright 2022 © SantaSpeen'

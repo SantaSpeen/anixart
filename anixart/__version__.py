e5768a6239b2a61fbcbaacb79976de8b25da86de
SantaSpeen <SantaSpeen@yandex.ru>
2022-09-27 18:10:19 +0300

    Создана общая структура библиотеки

---
+++ anixart/__version__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'anixart'
+__description__ = 'Wrapper for using the Anixart API.'
+__url__ = 'https://github.com/SantaSpeen/anixart'
+__version__ = '0.1'
+__build__ = 1
+__author__ = 'Maxim Khomutov'
+__author_email__ = 'SantaSpeen@gmail.com'
+__license__ = "FPA"
+__copyright__ = 'Copyright 2022 Maxim Khomutov'

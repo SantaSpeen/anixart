d9a577ee14d80dcf898ffc2095fa64eacbdc5ab4
SantaSpeen <santaspeen@yandex.ru>
2025-04-02 17:55:16 +0300

    [~] Ошибки поменялись?

--- anixart/enums.py
+++ anixart/enums.py
@@ -2,8 +2,8 @@ from enum import IntEnum
 
 class AnixartApiErrors(IntEnum):
     """ Error codes for AnixartApi authentication."""
-    INCORRECT_LOGIN = 1
-    INCORRECT_PASSWORD = 2
+    INCORRECT_LOGIN = 2
+    INCORRECT_PASSWORD = 3
 
 class AnixartComment(IntEnum):
     DISLIKE = 1

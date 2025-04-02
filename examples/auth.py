52ebb42aa2bdcfb2fb3b8dd5e5c4cb720732fb9e
SantaSpeen <santaspeen@yandex.ru>
2025-04-02 17:47:27 +0300

    [~] Промежуточный вариант

--- examples/auth.py
+++ examples/auth.py
@@ -1,9 +1,19 @@
-from anixart import AnixartAPI, AnixartUserAccount, PROFILE
+from anixart import AnixartAPI, AnixartAccount
+from anixart import endpoints
+from anixart.exceptions import AnixartAPIRequestError
+from anixart.profile import Profile
 
-user = AnixartUserAccount("SantaSpeen", "I_H@ve_Very_Secret_P@ssw0rd!")
-anix = AnixartAPI(user)
+anix = AnixartAPI()  # По умолчанию используется гость
 
+# acc = AnixartAccount("SantaSpeen", "I_H@ve_Very_Secret_P@ssw0rd!")
+# # id у аккаунта появляется только после
+# anix.use_account(acc)
 
 if __name__ == '__main__':
-    me = anix.execute("GET", PROFILE.format(user.id))
-    print(me.json())
+    try:
+        raw = anix.execute("GET", endpoints.PROFILE.format(1))
+        profile = {"is_my_profile": raw['is_my_profile'], **raw['profile']}
+        print(Profile(**profile))
+    except AnixartAPIRequestError as e:
+        print(e.message)
+        print(e.code)
